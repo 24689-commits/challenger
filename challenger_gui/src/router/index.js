603ca1bd0c35f19ffcfe43fec0d479a648c9c3cb
Aphelele2 <aphelelejoyi@gmail.com>
2023-08-18 13:42:07 +0200

    users

--- challenger_gui/src/router/index.js
+++ challenger_gui/src/router/index.js
@@ -14,7 +14,28 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/Products',
+    name: 'Products',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ProductsView.vue')
+  },
+  {
+    path: '/Contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ContactView.vue')
+  },
+  {
+    path: '/Login',
+    name: 'Login/Register',
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/users',
+    name: 'Users',
+    component: () => import(/* webpackChunkName: "about" */ '../views/UsersView.vue')
   }
+  
 ]
 
 const router = createRouter({
---
+++ challenger_gui/src/views/UsersView.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h2 class="display-2">Users</h2>
+        </div>
+        <div class="row justify-content-center gap-3" v-if="users">
+            <div v-for="user in users"  class="card" style="width: 18rem;" :key="user.userID">
+                <img :src="user.profileUrl" class="card-img-top" :alt="user.firstName">
+                <div class="card-body">
+                  <h5 class="card-title">{{user.firstName}} {{user.lastName}}</h5>
+                  <p class="card-text">
+                    <span>email address:{{user.emailAdd}}</span>
+                  </p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+        </div>
+        <div v-else class="row">
+            <SpinnerCompVue/>
+        </div>
+    </div>
+</template>
+<script>
+import SpinnerCompVue from '../components/SpinnerComp.vue'
+    export default {
+        components:{
+            SpinnerCompVue
+        },
+        computed:{
+            users(){
+                return this.$store.state.users
+            }
+        },
+        // rendering component to the browser
+        mounted(){
+            this.$store.dispatch('fetchUsers')
+        }
+    }
+</script>
+<style scoped>
+</style>
+
+
+
+
+
+
+
+
+
